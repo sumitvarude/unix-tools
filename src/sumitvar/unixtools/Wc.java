2715a10776351d1d8286d64198bdefd8ecc604a0
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2014-01-08 14:25:38 +0530

    wordcount competed

---
+++ src/sumitvar/unixtools/Wc.java
@@ -0,0 +1,20 @@
+package sumitvar.unixtools;
+
+
+class Wc{
+	public int CountChar(String fileData){
+        return fileData.split("").length;
+	}
+	public int CountLines(String fileData){
+        return fileData.split("\n").length;
+	}
+	public int CountWord(String fileData){
+        String lines[] =  fileData.split("\n");
+        int noOfWords = 0;
+        for(int i = 0;i<lines.length;i++){
+            String words[] = lines[i].split(" ");
+            noOfWords += words.length;
+        }
+        return noOfWords;
+    }
+}
---
+++ src/sumitvar/unixtools/WcClient.java
@@ -0,0 +1,25 @@
+package sumitvar.unixtools;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class WcClient{
+    public static void main(String[] args) {
+        String file = args[0];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            String fileData = "",line;
+            Wc operation = new Wc();
+            while ((line = br.readLine())!= null) {
+                fileData  += line+"\n";
+            }
+            System.out.println("file data:\n"+fileData);
+            int noOfChars = operation.CountChar(fileData);
+            int noOfLines = operation.CountLines(fileData);
+            int noOfWords = operation.CountWord(fileData);
+            System.out.println("	"+noOfLines+"	"+noOfWords+"	"+noOfChars+" "+file);
+        } catch (Exception e) {
+            System.out.println("file not found " + file);
+        }
+    }
+}
