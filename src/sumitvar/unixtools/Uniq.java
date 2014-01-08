b917394a89185315acff19208b366678dbde427a
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2014-01-09 00:32:27 +0530

    Uniq command completed

---
+++ src/sumitvar/unixtools/Uniq.java
@@ -0,0 +1,26 @@
+package sumitvar.unixtools;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Uniq {
+    public static void main(String[] args) {
+        String line,uniqLines = "",filename = args[0],fileData = "";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            System.out.println("file "+filename);
+
+            while ((line = br.readLine())!= null){
+                fileData += line + "\n";
+            }
+            String lines[] = fileData.split("\n");
+            for (int i = 1; i < lines.length; i++) {
+                if(lines[i-1].equals(lines[i])== false)
+                    uniqLines += lines[i]+"\n";
+            }
+            System.out.println(uniqLines);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
