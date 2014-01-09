56487c88ef4c96867b3b14ef3b7b9953c5b89944
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2014-01-09 12:10:49 +0530

    reduce spaces completd successFully

--- src/sumitvar/unixtools/ReduceSpaces.java
+++ src/sumitvar/unixtools/ReduceSpaces.java
@@ -1,4 +1,9 @@
 package sumitvar.unixtools;
 
 public class ReduceSpaces {
+    public static void main(String[] args) {
+        String fileData = File.read(args[0]);
+        fileData = fileData.replaceAll("[ ]+", " ");
+        System.out.println(fileData);
+    }
 }
