a27b224d91f94abc9f8d782ee3dc2037846d4726
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2014-01-09 10:54:43 +0530

    wc completed using ReadFile method

--- src/sumitvar/unixtools/WcClient.java
+++ src/sumitvar/unixtools/WcClient.java
@@ -1,19 +1,11 @@
 package sumitvar.unixtools;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class WcClient{
     public static void main(String[] args) {
         String file = args[0];
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String fileData = "",line;
+            String fileData = ReadFile.read(file);
             Wc operation = new Wc();
-            while ((line = br.readLine())!= null)
-                fileData += line + "\n";
-
-            System.out.println("file data:\n"+fileData);
             int noOfChars = operation.CountChar(fileData);
             int noOfLines = operation.CountLines(fileData);
             int noOfWords = operation.CountWord(fileData);
