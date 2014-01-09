1350311efe25dabcf5f944ee5706396765818604
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2014-01-09 10:48:23 +0530

    Uniq completed without try catch block

--- src/sumitvar/unixtools/Uniq.java
+++ src/sumitvar/unixtools/Uniq.java
@@ -6,17 +6,13 @@ import java.io.FileReader;
 public class Uniq {
     public static void main(String[] args) {
         String line,uniqLines = "",filename = args[0],fileData = "";
-        try{
-            fileData = ReadFile.read(filename);
-            String lines[] = fileData.split("\n");
-            uniqLines = lines[0]+"\n";
-            for (int i = 1; i < lines.length; i++) {
-                if(lines[i-1].equals(lines[i])== false)
-                    uniqLines += lines[i]+"\n";
-            }
-            System.out.println(uniqLines);
-        }catch(Exception e){
-            e.printStackTrace();
+        fileData = ReadFile.read(filename);
+        String lines[] = fileData.split("\n");
+        uniqLines = lines[0]+"\n";
+        for (int i = 1; i < lines.length; i++) {
+            if(lines[i-1].equals(lines[i])== false)
+                uniqLines += lines[i]+"\n";
         }
+        System.out.println(uniqLines);
     }
 }
