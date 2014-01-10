568e34851cce1e274c470977cfd86af5d3c18708
sumitvarude <sumitvarude@gmail.com>
2014-01-10 22:46:21 +0530

    used instance of class to call methods in UniqClient

--- src/sumitvar/unixtools/UniqClient.java
+++ src/sumitvar/unixtools/UniqClient.java
@@ -3,8 +3,9 @@ package sumitvar.unixtools;
 public class UniqClient {
     public static void main(String[] args){
         String line,uniqLines = "",filename = args[0],fileData = "";
-        fileData = File.read(filename);
         Uniq uniq = new Uniq();
+        FileOperations fs = new FileOperations();
+        fileData = fs.read(filename);
         uniqLines = uniq.getUniqLines(fileData);
         System.out.println(uniqLines);
     }
