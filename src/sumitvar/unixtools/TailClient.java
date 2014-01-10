041ddedeaddbcadd7f4006b1948d2f406e50c3b5
sumitvarude <sumitvarude@gmail.com>
2014-01-10 22:46:01 +0530

    used instance of class to call methods in TailClient

--- src/sumitvar/unixtools/TailClient.java
+++ src/sumitvar/unixtools/TailClient.java
@@ -3,14 +3,16 @@ package sumitvar.unixtools;
 public class TailClient {
     public static void main(String[] args) {
         int count;
+        FileOperations fs = new FileOperations();
+        Tail tail = new Tail();
         String line,tailLines = "",filename = args[0],fileData = "",value = "10";
         if(args[0].startsWith("-")) {
             value = args[0].substring(1);
             filename = args[1];
         }
         count = Integer.parseInt(value);
-        fileData = File.read(filename);
-        String tailData = Tail.getTail(count,fileData);
+        fileData = fs.read(filename);
+        String tailData = tail.getTail(count,fileData);
         System.out.println(tailData);
     }
 }
