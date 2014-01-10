a6034e325c9b97db9658d44cdd49574333ec4d69
sumitvarude <sumitvarude@gmail.com>
2014-01-10 22:49:30 +0530

    Class name changed from File to FileOperations

---
+++ src/sumitvar/unixtools/FileOperations.java
@@ -0,0 +1,19 @@
+package sumitvar.unixtools;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileOperations {
+    public static String read(String fileName){
+        String line,fileData = "";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while((line = br.readLine())!= null){
+                fileData += line + "\n";
+            }
+        }catch(Exception e){
+            System.out.println("file not found");
+        }
+        return fileData;
+    }
+}
