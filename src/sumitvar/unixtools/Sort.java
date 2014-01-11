847ee655b6d6eacf504a658ec4ec2cea1b07feb1
sumitvarude <sumitvarude@gmail.com>
2014-01-11 13:29:08 +0530

    Reverse method in sort completed

--- src/sumitvar/unixtools/Sort.java
+++ src/sumitvar/unixtools/Sort.java
@@ -12,4 +12,13 @@ public class Sort extends FileOperations{
         String combinedData = fs.getCombinedData(lines);
         return combinedData;
     }
+    public String reverse(String fileData){
+        FileOperations fs = new FileOperations();
+        String sortedData = sort(fileData);
+        String lines[] = fs.getLines(sortedData);
+        List<String> strList = Arrays.asList(lines);
+        Collections.reverse(strList);
+        lines = strList.toArray(new String[strList.size()]);
+        return fs.getCombinedData(lines);
+    }
 }
