45b9bdc47b4e1f534ca9b8ae8b5a87e278bf9648
sumitvarude <sumitvarude@gmail.com>
2014-01-11 13:30:07 +0530

    used reverse method in SortClient

--- src/sumitvar/unixtools/SortClient.java
+++ src/sumitvar/unixtools/SortClient.java
@@ -6,7 +6,10 @@ public class SortClient {
         FileOperations fs = new FileOperations();
         String fileData = fs.read(args[0]);
 
-        String SortedData = sort.sort(fileData);
-        System.out.println(SortedData);
+        String sortedData = sort.sort(fileData);
+        System.out.println("SortedData:\n"+sortedData);
+
+        String reversedData = sort.reverse(fileData);
+        System.out.println("ReversedData:\n"+reversedData);
     }
 }
