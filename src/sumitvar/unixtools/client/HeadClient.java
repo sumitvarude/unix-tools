68706c055cfa9ca1aa5393a6819a96a7f2f93b71
sumitvarude <sumitvarude@gmail.com>
2014-01-27 09:39:16 +0530

    HeadClient & TailClient configured with config.properties file

--- src/sumitvar/unixtools/client/HeadClient.java
+++ src/sumitvar/unixtools/client/HeadClient.java
@@ -3,6 +3,9 @@ package sumitvar.unixtools.client;
 import sumitvar.unixtools.FileOperations;
 import sumitvar.unixtools.Head;
 
+import java.io.FileReader;
+import java.util.Properties;
+
 public class HeadClient {
     public static void main(String[] args) {
         int count;
@@ -10,6 +13,7 @@ public class HeadClient {
             System.out.println("please give file name");
             return;
         }
+
         FileOperations fs = new FileOperations();
         Head head = new Head();
         String line, filename = args[0], fileData = "", value = "10";
@@ -17,6 +21,14 @@ public class HeadClient {
         if (args[0].startsWith("-")) {
             value = args[0].substring(1);
             filename = args[1];
+        }
+        try  {
+            FileReader reader = new FileReader("config.properties");
+            Properties properties = new Properties();
+            properties.load(reader);
+            value = (properties.getProperty("head.defaultNoOfLines"));
+        } catch (Exception e) {
+            value = "10";
         }
         count = Integer.parseInt(value);
         fileData = fs.read(filename);
--- src/sumitvar/unixtools/client/TailClient.java
+++ src/sumitvar/unixtools/client/TailClient.java
@@ -3,6 +3,9 @@ package sumitvar.unixtools.client;
 import sumitvar.unixtools.FileOperations;
 import sumitvar.unixtools.Tail;
 
+import java.io.FileReader;
+import java.util.Properties;
+
 public class TailClient {
     public static void main(String[] args) {
         int count;
@@ -16,6 +19,14 @@ public class TailClient {
         if (args[0].startsWith("-")) {
             value = args[0].substring(1);
             filename = args[1];
+        }
+        try  {
+            FileReader reader = new FileReader("config.properties");
+            Properties properties = new Properties();
+            properties.load(reader);
+            value = (properties.getProperty("tail.defaultNoOfLines"));
+        } catch (Exception e) {
+            value = "10";
         }
         count = Integer.parseInt(value);
         fileData = fs.read(filename);
